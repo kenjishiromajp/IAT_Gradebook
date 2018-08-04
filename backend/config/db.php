cc9952c76df3bf6b524ecc754437d8be4377e327
Giovanni Kenji Shiroma <kenjishiromajp@gmail.com>
2018-08-04 11:40:27 -0300

    Preparing the default authentication for database

--- backend/config/db.php
+++ backend/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.64.2;dbname=gradebook',
-    'username' => 'monty',
-    'password' => 'some_pass',
+    'dsn' => 'mysql:host=localhost;dbname=gradebook',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
