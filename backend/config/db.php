e3aeb76871d4434246ec02e6fa0b2ca5f12535e9
Giovanni Kenji Shiroma <kenjishiromajp@gmail.com>
2018-08-04 11:45:25 -0300

    Database credentials change

--- backend/config/db.php
+++ backend/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=192.168.64.2;dbname=gradebook',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'monty',
+    'password' => 'some_pass',
     'charset' => 'utf8',
 ];
