c810aeda3485799a3013b2c6d171aeab5169a86b
Giovanni Kenji Shiroma <kenjishiromajp@gmail.com>
2018-08-04 11:41:48 -0300

    Database credentials for development

--- backend/config/db.php
+++ backend/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=gradebook',
+    'dsn' => 'mysql:host=192.168.64.2;dbname=gradebook',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
