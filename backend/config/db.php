1d862beae3c5ac0d93ee127bca8356268a0ab622
Giovanni Kenji Shiroma <kenjishiromajp@gmail.com>
2018-08-04 13:17:50 -0300

    Changing database config

--- backend/config/db.php
+++ backend/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=gradebook',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=192.168.64.2;dbname=gradebook',
+    'username' => 'monty',
+    'password' => 'some_pass',
     'charset' => 'utf8',
 ];
